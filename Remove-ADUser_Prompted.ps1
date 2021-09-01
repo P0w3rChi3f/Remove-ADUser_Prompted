d2cdaf9caccaf53c6d9188dd66b37665ae7cf2d4
P0w3rChi3f <honeycutt.james@gmail.com>
2021-08-31 18:35:55 -0700

    Minor changes made

--- Remove-ADUser_Prompted.ps1
+++ Remove-ADUser_Prompted.ps1
@@ -8,19 +8,19 @@
 ###########################################
 
 
-        $DODID = read-host "What is the users DODID # "
-        $User = Get-ADUser -Filter {employeeID -like $DODID} -searchbase "OU=SubOU3,OU=SubOU2,OU=SubOU1,DC=SubDomain1,DC=SubDomain2,DC=SubDomain3,DC=com" -Verbose      #-OtherAttributes @{'employeeID'=$user}  
+        $DODID = read-host "What is the users ID # "
+        $User = Get-ADUser -Filter {employeeID -like $ID} -searchbase "OU=SubOU3,OU=SubOU2,OU=SubOU1,DC=SubDomain1,DC=SubDomain2,DC=SubDomain3,DC=com" -Verbose      #-OtherAttributes @{'employeeID'=$user}  
         $samAccount = $($user.SamAccountName)
-        $Path = "\\ngpea7-fl0-1\Profiles\$samAccount"
+        $Path = "\\<FileSVRName>\Profiles\$samAccount"
 
         #unassign the user from their virtual machine (This portion does not work when run as a script.  It can be copied and pasted into a PowerShell Console and it will work.)
        <# 
         
         try {
-            Enter-PSSession –Computername ngpea0viewbroke.ng.ds.army.mil #–credential (get-credential)
+            Enter-PSSession –Computername ngpea0viewbroke.fqdn.com #–credential (get-credential)
             Get-PSSnapin -name VMware* -Registered | Add-PSSnapin
             Add-PSSnapin -Name 
-            Connect-VIServer 55.195.251.50 
+            Connect-VIServer <IPAddress> 
 
             $vmDesktop = Get-DesktopVM | Where-Object {$_.user_sid -eq $(get-user -Name $User.sid)}
             Remove-UserOwnership -Machine_id $vmDesktop.machine_id -Verbose
